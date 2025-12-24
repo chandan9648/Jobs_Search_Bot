c242ffa7bae071891acaba37fcd5b90f96abf886
Chandan Chaudhary <chandankkkumar156@gmail.com>
2025-12-24 15:44:28 +0530

    fixing package file

--- backend/server.js
+++ backend/server.js
@@ -18,6 +18,7 @@ cron.schedule("0 13 * * *", async () => {
   timezone: "Asia/Kolkata"
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
