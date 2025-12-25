bc4b724379d601a74fd027cfd613eb8387485754
Chandan Chaudhary <chandankkkumar156@gmail.com>
2025-12-25 18:25:30 +0530

    testing server.js file

--- backend/server.js
+++ backend/server.js
@@ -1,24 +1,18 @@
 const dotenv = require("dotenv");
 dotenv.config();
-const cron = require("node-cron");
+
 const searchJobs = require("./searchJobs");
 const sendEmail = require("./sendEmails");
-const express = require("express");
-const app = express();
 
-
-
-// Runs every day at 1:00 PM IST
-cron.schedule("0 13 * * *", async () => {
-  console.log("Searching jobs...");
-  const jobs = await searchJobs();
-  await sendEmail(jobs);
-  console.log("Email sent successfully");
-}, {
-  timezone: "Asia/Kolkata"
-});
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+(async () => {
+  try {
+    console.log("Searching jobs...");
+    const jobs = await searchJobs();
+    await sendEmail(jobs);
+    console.log("Email sent successfully");
+    process.exit(0);
+  } catch (error) {
+    console.error("Error:", error);
+    process.exit(1);
+  }
+})();
